16a62ba06f4bba0c7884e9418143591c93a0fa2e
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-04-23 18:06:33 -0300

    feat: :sparkles: Request de login
    Classe de request de login adicionado
    
    #8

---
+++ back/estagiou/src/main/java/com/lab/estagiou/controller/dto/request/auth/RequestAuthentication.java
@@ -0,0 +1,11 @@
+package com.lab.estagiou.controller.dto.request.auth;
+
+import lombok.Data;
+
+@Data
+public class RequestAuthentication {
+
+    private String email;
+    private String senha;
+    
+}
