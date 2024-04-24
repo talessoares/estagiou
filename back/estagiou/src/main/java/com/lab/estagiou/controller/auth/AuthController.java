f32f4e48efa7d1c6011ecc1e7f48c6b5662fddfb
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-04-24 19:46:01 -0300

    refactor: :fire: Remoção de código
    Remoção de código para as classes específicas
    
    #10

--- back/estagiou/src/main/java/com/lab/estagiou/controller/auth/AuthController.java
+++ back/estagiou/src/main/java/com/lab/estagiou/controller/auth/AuthController.java
@@ -2,34 +2,27 @@ package com.lab.estagiou.controller.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.lab.estagiou.controller.dto.request.auth.RequestAuthentication;
-import com.lab.estagiou.controller.dto.request.auth.RequestCadastro;
+
 import com.lab.estagiou.service.auth.AuthorizationService;
 
 import jakarta.validation.Valid;
 
-@RestController
+@Controller
 @RequestMapping("/v1/auth")
 public class AuthController {
    
     @Autowired
-    AuthorizationService authorizationService;
+    private AuthorizationService authorizationService;
 
     @PostMapping("/login")
     public ResponseEntity<Object> login(@RequestBody @Valid RequestAuthentication authetinticationDto) {
         return authorizationService.login(authetinticationDto);
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<Object> register (@RequestBody RequestCadastro registerDto, Authentication authentication) {
-        return authorizationService.register(registerDto, authentication);
     }
 
 }
--- back/estagiou/src/main/java/com/lab/estagiou/service/auth/AuthorizationService.java
+++ back/estagiou/src/main/java/com/lab/estagiou/service/auth/AuthorizationService.java
@@ -9,20 +9,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.lab.estagiou.controller.dto.request.auth.RequestCadastro;
 import com.lab.estagiou.controller.dto.request.auth.RequestAuthentication;
 import com.lab.estagiou.controller.dto.response.auth.LoginResponse;
-import com.lab.estagiou.controller.dto.response.error.ErrorResponse;
-import com.lab.estagiou.model.entity.Admin;
-import com.lab.estagiou.model.entity.Aluno;
-import com.lab.estagiou.model.entity.Empresa;
+
 import com.lab.estagiou.model.entity.Usuario;
-import com.lab.estagiou.model.repository.EmpresaRepository;
+
 import com.lab.estagiou.model.repository.UsuarioRepository;
+
 import com.lab.estagiou.security.TokenService;
 
 import jakarta.validation.Valid;
@@ -39,9 +35,6 @@ public class AuthorizationService implements UserDetailsService {
     @Autowired
     private TokenService tokenService;
 
-    @Autowired
-    private EmpresaRepository empresaRepository;
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return usuarioRepository.findByEmail(email);
@@ -56,54 +49,6 @@ public class AuthorizationService implements UserDetailsService {
         String token = tokenService.generateToken((Usuario) auth.getPrincipal());
 
         return ResponseEntity.ok(new LoginResponse(token));
-    }
-
-    public ResponseEntity<Object> register(@RequestBody RequestCadastro registerDto, Authentication authentication) {
-        if (usuarioRepository.findByEmail(registerDto.getEmail()) != null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        String senhaEncriptada = new BCryptPasswordEncoder().encode(registerDto.getSenha());
-
-        try {
-            registerDto.validar();
-        } catch (RuntimeException e) {
-            return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
-        }
-
-        try {
-            Usuario usuario = createUser(registerDto, senhaEncriptada, authentication);
-            
-            if (usuario != null) {
-                usuarioRepository.save(usuario);
-                return ResponseEntity.ok().build();
-            } 
-
-            return ResponseEntity.badRequest().build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
-        }
-    }
-
-    private Usuario createUser(RequestCadastro registerDto, String senhaEncriptada, Authentication authentication) {
-        switch (registerDto.getRole()) {
-            case "ALUNO":
-                return new Aluno(registerDto, senhaEncriptada);
-            case "EMPRESA":
-                if (authentication.getPrincipal() instanceof Admin
-                        && !empresaRepository.existsByCnpj(registerDto.getCnpj())) {
-                    return new Empresa(registerDto, senhaEncriptada);
-                }
-                break;
-            case "ADMIN":
-                if (authentication.getPrincipal() instanceof Admin) {
-                    return new Admin(registerDto, senhaEncriptada);
-                }
-                break;
-            default:
-                break;
-        }
-        return null;
     }
 
 }
