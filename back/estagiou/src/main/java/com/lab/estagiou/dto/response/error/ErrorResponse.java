a90db0d8177aa89513d111d7898e5a26557f6249
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-05-06 18:08:15 -0300

    refactor: :recycle: Adição de novos atributos para o error response
    Adição de novos atributos para o error response

--- back/estagiou/src/main/java/com/lab/estagiou/dto/response/error/ErrorResponse.java
+++ back/estagiou/src/main/java/com/lab/estagiou/dto/response/error/ErrorResponse.java
@@ -1,13 +1,25 @@
 package com.lab.estagiou.dto.response.error;
 
-import lombok.AllArgsConstructor;
+import java.time.Instant;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class ErrorResponse {
 
+    private Instant timestamp;
     private int status;
+    private String error;
     private String message;
-    
+    private String path;
+
+    public ErrorResponse(int status, String message, HttpServletRequest request) {
+        this.timestamp = Instant.now();
+        this.status = status;
+        this.error = "Erro";
+        this.message = message;
+        this.path = request.getRequestURI();
+    }
+
 }
