f4a0abaa42da190a0c26d7f2d4a03bc42167357b
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-05-21 22:35:50 -0300

    refactor: :recycle: Alteração na request

--- back/estagiou/src/main/java/com/lab/estagiou/dto/request/model/student/StudentRegisterRequest.java
+++ back/estagiou/src/main/java/com/lab/estagiou/dto/request/model/student/StudentRegisterRequest.java
@@ -1,13 +1,13 @@
 package com.lab.estagiou.dto.request.model.student;
 
-import com.lab.estagiou.dto.request.model.util.RequestRegister;
+import com.lab.estagiou.dto.request.model.util.RequestEmailRegisterAddress;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class StudentRegisterRequest extends RequestRegister {
+public class StudentRegisterRequest extends RequestEmailRegisterAddress {
 
     private String lastName;
     
