65d96c2ea80100464ae7d9f7135de67f9270290b
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-05-21 23:01:24 -0300

    refactor: :recycle: Criação e alteração de novos handlers

---
+++ back/estagiou/src/main/java/com/lab/estagiou/exception/handler/SendFailedExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.lab.estagiou.exception.handler;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.lab.estagiou.dto.response.error.ErrorResponse;
+import com.lab.estagiou.exception.handler.util.HandlerExceptionUtil;
+import com.lab.estagiou.model.log.LogEnum;
+
+import jakarta.mail.SendFailedException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class SendFailedExceptionHandler extends HandlerExceptionUtil {
+
+    @ExceptionHandler(value = SendFailedException.class)
+    public ResponseEntity<Object> handleException(Exception e, HttpServletRequest request) {
+        logger(LogEnum.ERROR, "Internal error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), request);
+        return ResponseEntity.internalServerError().body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Erro interno", request));
+    }
+    
+}
