d6771ada484de2d8b30f44c5ccf625e180937b85
Ricardo Costa <ricardo_hentges@hotmail.com>
2024-05-24 19:06:21 -0300

    docs: :memo: Alterado swagger do student
    Alterado swagger do student

--- back/estagiou/src/main/java/com/lab/estagiou/controller/StudentController.java
+++ back/estagiou/src/main/java/com/lab/estagiou/controller/StudentController.java
@@ -55,7 +55,6 @@ public class StudentController {
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Students listed successfully"),
         @ApiResponse(responseCode = "204", description = "No students found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "401", description = "Authentication expired", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "403", description = "User not authorized", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
@@ -68,7 +67,6 @@ public class StudentController {
     @Operation(summary = "Search student by ID", description = "Search a student by ID")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Student found successfully", content = @Content(schema = @Schema(implementation = StudentEntity.class))),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "401", description = "Authentication expired", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "403", description = "User not authorized", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "404", description = "Student not found", content = @Content),
@@ -95,11 +93,11 @@ public class StudentController {
     @Operation(summary = "Update student", description = "Update a student")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "204", description = "Student updated successfully", content = @Content),
-        @ApiResponse(responseCode = "400", description = "Bad request", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(responseCode = "401", description = "Incorret atributtes", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "400", description = "Incorrect atributtes", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "401", description = "Authentication expired", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
         @ApiResponse(responseCode = "403", description = "User not authorized", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(responseCode = "404", description = "Student not found", content = @Content),
-        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+        @ApiResponse(responseCode = "404", description = "Student not found", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
     })
     @PutMapping("/{id}")
     public ResponseEntity<Object> updateStudent(@PathVariable UUID id, @RequestBody StudentRegisterRequest requestCadastro, Authentication authentication) {
